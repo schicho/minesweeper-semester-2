af3a4b3ba7e6b5262b23cd2812e094f8db770c6b
HoeferSi <hoefer.simi@googlemail.com>
2020-06-05 23:35:15 +0200

    added CLI-prototype

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+public class main {
+    //Diese Main Klasse hat nichts mit der eigentlichen zutun und ist zur zum Testen hier.
+
+    public static void main(String[] args) {
+        System.out.println("hey");
+        Cli test = new Cli();
+
+    }
+}
