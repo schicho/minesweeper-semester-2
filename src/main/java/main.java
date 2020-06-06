80270900727d7c19356728a7f7d6fb3738fe5c44
Höfer Simon <hoefer.simi@googlemail.com>
2020-06-06 14:49:39 +0200

    added budget Eventhandler for testing

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,12 @@
+import java.util.logging.Handler;
+
 public class main {
     //Diese Main Klasse hat nichts mit der eigentlichen zutun und ist zur zum Testen hier.
 
     public static void main(String[] args) {
         System.out.println("hey");
         Cli test = new Cli();
-
+        handler Handler = new handler(test);
+        test.startGame();
     }
 }
