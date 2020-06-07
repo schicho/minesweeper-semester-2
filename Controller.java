52a22b9e624a4bf44da8c800c1a811fb08c88bc6
3nc0der <31252187+3nc0der@users.noreply.github.com>
2020-06-07 09:28:54 +0200

    Added class Controller
    Theres nothing implemented yet, just the classes basic structure.

---
+++ Controller.java
@@ -0,0 +1,33 @@
+package main.java;
+
+class Model{
+
+}
+
+public class Controller {
+
+    /**
+     * creates a new controller instance, which is used to handle input
+     * and update the model it is given respectively
+     */
+    public Controller(){
+
+    }
+
+    /**
+     * updates the given model instance dependent of the input
+     * the class received earlier
+     *
+     * @param model
+     */
+    public void updateModel(Model model){
+
+    }
+
+    /**
+     * interprets the given input and takes action dependent of this
+     */
+    public void handleInput(){
+
+    }
+}
