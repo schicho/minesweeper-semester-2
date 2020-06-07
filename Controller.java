8f5efa0d6cdeb2651e26d3d079272d2843f09578
3nc0der <31252187+3nc0der@users.noreply.github.com>
2020-06-07 11:02:13 +0200

    Updated controller class
    it can handle key input and updates the model (this has to be connected with the real model class asap)
    also theres some code commented out for mouse input with the awt mouselistner class

--- Controller.java
+++ Controller.java
@@ -1,10 +1,43 @@
 package main.java;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
 class Model{
 
+    DIFFICULTY difficulty;
+
+    void sweepTile(int row, int col){
+
+    }
 }
 
-public class Controller {
+enum DIFFICULTY{
+    EASY,
+    MEDIUM,
+    HARD
+}
+
+public class Controller /*implements MouseListener*/ {
+
+    /**
+     * the string that will be interpreted in updateModel
+     * is filled with data by handleInput
+     */
+    private String command;
+
+    /**
+     * these are containing the evaluated step values
+     * used to update the model
+     */
+    private int m, n;
+
+    /**
+     * scans the next line (command)
+     */
+    private Scanner scanner;
 
     /**
      * creates a new controller instance, which is used to handle input
@@ -12,6 +45,7 @@ public class Controller {
      */
     public Controller(){
 
+        scanner = new Scanner(System.in);
     }
 
     /**
@@ -22,12 +56,43 @@ public class Controller {
      */
     public void updateModel(Model model){
 
+        handleInput();
+
+        model.sweepTile(m, n);
     }
 
     /**
-     * interprets the given input and takes action dependent of this
+     * interprets the given input
      */
-    public void handleInput(){
+    private void handleInput(){
+
+        //read the next command from user
+        command = scanner.nextLine();
+
+        //read out step values
+        String[] parts = command.split(":");
+        m = Integer.parseInt(parts[0]);
+        n = Integer.parseInt(parts[1]);
+    }
+
+    /*
+    /**
+     * overrides keyTyped method from KeyListener
+     * @param e
+     *
+    @Override
+    public void keyTyped(KeyEvent e) {
+        lastInput = e.getKeyChar();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
 
     }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+    */
 }
