0d095875d892c6cc742602d3888d2305c7801772
3nc0der <31252187+3nc0der@users.noreply.github.com>
2020-06-08 11:57:33 +0200

    added controller in right folder

---
+++ src/main/java/Controller.java
@@ -0,0 +1,81 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
+public class Controller /*implements MouseListener*/ {
+
+    /**
+     * the string that will be interpreted in updateModel
+     * is filled with data by handleInput
+     */
+    private String command;
+
+    /**
+     * these are containing the evaluated step values
+     * used to update the model
+     */
+    private int m, n;
+
+    /**
+     * scans the next line (command)
+     */
+    private Scanner scanner;
+
+    /**
+     * creates a new controller instance, which is used to handle input
+     * and update the model it is given respectively
+     */
+    public Controller(){
+
+        scanner = new Scanner(System.in);
+    }
+
+    /**
+     * updates the given model instance dependent of the input
+     * the class received earlier
+     *
+     * @param model
+     */
+    public void updateModel(Model model){
+
+        handleInput();
+
+        model.sweepTile(m, n);
+    }
+
+    /**
+     * interprets the given input
+     */
+    private void handleInput(){
+
+        //read the next command from user
+        command = scanner.nextLine();
+
+        //read out step values
+        String[] parts = command.split(":");
+        m = Integer.parseInt(parts[0]);
+        n = Integer.parseInt(parts[1]);
+    }
+
+    /*
+    /**
+     * overrides keyTyped method from KeyListener
+     * @param e
+     *
+    @Override
+    public void keyTyped(KeyEvent e) {
+        lastInput = e.getKeyChar();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+    */
+}
