7ed1a22e3828acdea6ae10f95e8cff6b79add6e5
cz999 <64591023+cz999@users.noreply.github.com>
2020-06-28 20:56:27 +0200

    user instruction extended

--- src/main/java/view/Cli.java
+++ src/main/java/view/Cli.java
@@ -38,7 +38,7 @@ public class Cli {
         drawModel(minefield);
 
         System.out.println("Wähle ein Feld mit dem Schema \"m:n\", um es aufzudecken.");
-        System.out.println("Füge ein f vor \"m:n\" an, um eine Flagge zu setzen oder wieder zu entfernen. Bsp: \"f4:2\".");
+        System.out.println("Füge ein f vor \"m:n\" an, um eine Flagge zu setzen, ein Fragezeichen zu setzen oder wieder zu entfernen. Bsp: \"f4:2\".");
         System.out.println("Mit \"ng\" wird jederzeit ein neues Spiel gestartet, während \"exit\" das Spiel sofort verlässt.");
     }
 
