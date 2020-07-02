ea80e76a16a567f2bd5aec9e5f8e1a8e2792f745
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2020-07-02 13:25:51 +0200

    cleanup controller class.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,11 +1,7 @@
 package controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.sql.SQLOutput;
-import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,7 +12,6 @@ import model.exceptions.*;
 import model.timer.*;
 
 import javax.swing.*;
-
 
 public class Controller implements MouseListener {
 
@@ -29,11 +24,6 @@ public class Controller implements MouseListener {
      * holds the model instance
      */
     private static Model model;
-
-    /**
-     * holds the cli instance
-     */
-    private static Cli cli;
 
     /**
      * holds the gui instance
@@ -53,6 +43,7 @@ public class Controller implements MouseListener {
 
     /**
      * Main game loop which runs the game and stops it at win or failure.
+     *
      * @param args *no arguments*
      */
     public static void main(String[] args) {
@@ -64,14 +55,18 @@ public class Controller implements MouseListener {
         //run model.timer ever 1000ms = 1s
         timer.schedule(timerTask, 0, 1000);
 
+        // TODO: this needs to be set by the main menu when starting a game.
         model = new Model(Difficulty.EASY);
         gui.calculateSize(model);
-        gui.loadScene(GameState.RUNNING); // MAIN_MENU should actually redirect here.
+        gui.loadScene(GameState.RUNNING);
 
         do {
             //check first time in case a new game was started and old
             //thread needs to be stopped
             if (exit) {
+                timerTask.cancel();
+                timerTask = null;
+                timer.cancel();
                 return;
             }
 
@@ -115,7 +110,7 @@ public class Controller implements MouseListener {
                     System.out.println("yalP");
                 } else System.out.println("Play");
 
-            // Click on Tile Button.
+            // Click on Tile Button. Sweep/Flag/Unflag
             } else if (e.getSource() instanceof TileButton) {
                 if (SwingUtilities.isRightMouseButton(e)) {
                     whatsItDo = "f" + whatsItDo;
@@ -161,28 +156,23 @@ public class Controller implements MouseListener {
     }
 
     /**
-     * saves the difficulty given by the user
-     */
-    private Difficulty difficulty;
-
-    /**
-     * tests userinput for all kinds of mistakes
-     */
-    private final InputExceptionHandler tester = new InputExceptionHandler();
-
-    /**
      * creates a new controller instance
      */
     public Controller() {
     }
 
     /**
-     * @return listener instance
+     * @return the current Controller which is also our inputHandler
      */
-
-
     public static Controller getMouseHandler() {
         return controller;
+    }
+
+    /**
+     * @return the current model
+     */
+    public static Model getModel() {
+        return model;
     }
 
 
@@ -194,80 +184,24 @@ public class Controller implements MouseListener {
         //index of row and column
         int m, n;
 
-            //flag a tile
-            if (input.contains(":") && input.startsWith("f")) {
-                input = input.replace("f", "");
-                //read out step values
-                String[] parts = input.split(":");
-                m = Integer.parseInt(parts[0]);
-                n = Integer.parseInt(parts[1]);
+        //flag a tile
+        if (input.contains(":") && input.startsWith("f")) {
+            input = input.replace("f", "");
+            //read out step values
+            String[] parts = input.split(":");
+            m = Integer.parseInt(parts[0]);
+            n = Integer.parseInt(parts[1]);
 
-                model.flagTile(m, n);
-            }
-            //sweep a tile
-            else if (input.contains(":")) {
-                //read out step values
-                String[] parts = input.split(":");
-                m = Integer.parseInt(parts[0]);
-                n = Integer.parseInt(parts[1]);
-
-                model.sweepTile(m, n);
-            } else {
-                //its not a mine command
-                switch (input) {
-                    case "ng": {
-                        //stop timerTask, set null and reset
-                        timerTask.cancel();
-                        timerTask = null;
-                        SecondsTimer.counter = 0;
-
-                        String[] noargs = {""};
-                        main(noargs);
-                    }
-                    break;
-                    case "exit": {
-                        //exit
-                        //timer needs to be stopped here, otherwise program wont terminate
-                        timer.cancel();
-                        exit = true;
-                    }
-                    break;
-                }
-            }
+            model.flagTile(m, n);
         }
+        //sweep a tile
+        else if (input.contains(":")) {
+            //read out step values
+            String[] parts = input.split(":");
+            m = Integer.parseInt(parts[0]);
+            n = Integer.parseInt(parts[1]);
 
-    /**
-     * Reads the difficulty the player wants to play the game in.
-     * Is entered and read at game start.
-     *
-     * @return Difficulty (value of enum)
-     */
-    private Difficulty readDifficulty() {
-        Scanner scanner = new Scanner(System.in);
-        Difficulty difficulty = null;
-        while (difficulty == null) {
-            String difficultyString = scanner.nextLine();
-            try {
-                tester.testForDifficulty(difficultyString);
-                switch (difficultyString.toLowerCase().trim()) {
-                    case "easy":
-                        difficulty = Difficulty.EASY;
-                        break;
-                    case "normal":
-                        difficulty = Difficulty.NORMAL;
-                        break;
-                    case "hard":
-                        difficulty = Difficulty.HARD;
-                        break;
-                }
-            } catch (NotADifficultyException e) {
-                System.out.println(e.toString());
-            }
+            model.sweepTile(m, n);
         }
-        return difficulty;
-    }
-
-    public static Model getModel() {
-        return model;
     }
 }
